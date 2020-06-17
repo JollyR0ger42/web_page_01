cbe861e23a5194474fd7f972d2b50f1b2b9338bc
Alex Shmidt <kav993@gmail.com>
2020-06-17 11:42:03 +0200

    add simple carousel for team members

--- script.js
+++ script.js
@@ -26,3 +26,61 @@ for(let el of filters){
 		event.target.classList.add('active-filter')
 	})
 }
+
+
+function paginate(collection, len){
+	const result = [];
+	for(let i = 0; i < collection.length; i += len){
+		result.push(Array.prototype.slice.call(collection, i, i+len))
+	}
+	return result
+}
+
+function showPage(cardsArray, pageNumber){
+	cardsArray.forEach((page, index) => {
+		if(index === pageNumber){
+			page.forEach(card => {
+				card.classList.remove('display-hidden')
+			})
+		} else {
+			page.forEach(card => {
+				card.classList.add('display-hidden')
+			})
+		}
+	})
+}
+
+function createDots(dotsElement, amount){
+	const dot = dotsElement.children[0];
+	const dotsArray = [];
+	dot.remove()
+
+	for(let i = 0; i < amount; i++){
+		let newDot = dot.cloneNode(true);
+		newDot.setAttribute('data-page', i)
+		dotsElement.appendChild(newDot)
+		dotsArray.push(newDot)
+	}
+	dotsArray[0].classList.add('active-dot')
+	return dotsArray
+}
+
+function carousel({target, cardsInBlock, dotsClass}){
+	const cardsCollection = document.getElementsByClassName(target)[0].children;
+	const dotsElement = document.getElementsByClassName(dotsClass)[0];
+
+	const cardsArray = paginate(cardsCollection, cardsInBlock);
+	const dotsArray = createDots(dotsElement, cardsArray.length);
+
+	showPage(cardsArray, 0)
+	dotsArray.forEach(dot => {
+		dot.addEventListener('click', event => {
+			dotsArray.forEach(dot => dot.classList.remove('active-dot'))
+			event.target.classList.add('active-dot')
+			let page = parseInt(event.target.getAttribute('data-page'));
+			showPage(cardsArray, page)
+		})
+	})
+}
+
+carousel({target: 'team', cardsInBlock: 3, dotsClass: 'nav-dots'})
